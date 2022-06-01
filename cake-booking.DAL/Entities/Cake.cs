bd9faf1fdd8bb72f4a10ea29165a8bf409299fc8
Raluca Fritz <fritzraluca@gmail.com>
2022-06-01 16:58:02 +0300

    added PickUpOrderTable and ScheduleTable

--- cake-booking.DAL/Entities/Cake.cs
+++ cake-booking.DAL/Entities/Cake.cs
@@ -9,9 +9,9 @@ namespace cake_booking.DAL.Entities
     public class Cake
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
         public virtual ICollection<PickUpOrder> PickUpOrders { get; set; }
     }
 }
